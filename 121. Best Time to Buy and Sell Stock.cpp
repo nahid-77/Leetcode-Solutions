38a32068a931ae96624af4bc941656d9b8fd72db
Nahid <mdnahidhosentuhin@gmail.com>
2026-05-09 21:27:55 +0600

    121 - Best time to buy and sell Stock

---
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+    public: 
+        int maxProfit(vector<int>& prices) {
+            int minPrice = prices[0];
+            int maxProfit = 0;
+
+            for(int i=1; i<prices.size(); i++) {
+                int profit = prices[i] - minPrice;
+
+                minPrice = min(minPrice, prices[i]);
+                maxProfit = max(maxProfit, profit);
+            };
+
+            return maxProfit;
+     }
+};
