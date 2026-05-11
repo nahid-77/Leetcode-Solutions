fb98311b0f34214f76bbd874a9dba146e01e1987
Nahid <mdnahidhosentuhin@gmail.com>
2026-05-11 06:29:03 +0600

    53 - Maximum SubArray

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution{
+    public:
+            int maxSubArray(vector<int>& nums) {
+            int currSum = nums[0];
+            int maxSum = nums[0];
+
+            for(int i = 1; i<nums.size(); i++){
+                currSum = max(nums[i], nums[i] + currSum);
+                maxSum = max(currSum, maxSum);
+            }
+            return maxSum;
+        }
+};
